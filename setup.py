f1edf3a89bc750dcd2d497cf363aa42a9a404fb3
dsamudre <dsamudre1994@gmail.com>
2025-09-02 01:42:20 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Boktiyar ahmad bappy",
+    author_email="dsamudre1995@gmail.com",
+    packages=find_packages(),
+    install_requires=[ ])
